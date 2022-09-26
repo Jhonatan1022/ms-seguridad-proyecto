505adc03841e87f7c2d9649ef350cb48378caa5d
jhonatan <jhonatan.1701322237@ucaldas.edu.co>
2022-09-25 20:51:16 -0500

    creacion de modelos,controladores y relaciones

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property, hasOne} from '@loopback/repository';
+import {Rol} from './rol.model';
+
+@model({settings: {strict: false}})
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @hasOne(() => Rol)
+  rol: Rol;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
